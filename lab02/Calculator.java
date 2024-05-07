009ee2244158c2c091bb9d2cd28bf164ff356eb3
shanccom <139720451+shanccom@users.noreply.github.com>
2024-05-07 11:47:11 -0500

    agrega metodo sub

--- lab02/Calculator.java
+++ lab02/Calculator.java
@@ -1,6 +1,8 @@
 public class Calculator {
   int add(int a, int b){ return 0; }
-  int sub(int a, int b){ return 0; }
+  int sub(int a, int b){
+    return a - b;
+  }
   int mul(int a, int b){ return 0; }
   int div(int a, int b){ return 0; }
   int mod(int a, int b){ return 0; }
