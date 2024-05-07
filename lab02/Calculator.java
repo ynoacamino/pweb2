0add9a3eff21deb309d8b7d29a6dd582a438a7d6
Yenaro Joel Noa Camino <97652778+ynoacamino@users.noreply.github.com>
2024-05-07 11:45:55 -0500

    agrega el metodo add()

--- lab02/Calculator.java
+++ lab02/Calculator.java
@@ -1,5 +1,8 @@
 public class Calculator {
-  int add(int a, int b){ return 0; }
+  int add(int a, int b){
+    return a + b;
+  }
+
   int sub(int a, int b){ return 0; }
   int mul(int a, int b){ return 0; }
   int div(int a, int b){ return 0; }
