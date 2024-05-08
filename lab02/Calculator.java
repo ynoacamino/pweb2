510f76cd76216a9c57e607414254d6b622b93e20
Yenaro Joel Noa Camino <97652778+ynoacamino@users.noreply.github.com>
2024-05-08 03:26:39 +0000

    arregla metodo mul()

--- lab02/Calculator.java
+++ lab02/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
   int sub(int a, int b){
     return a - b;
   }
-  int mul(int a, int b){ return 0; }
+  int mul(int a, int b){ return a * b; }
   int div(int a, int b){
     if (b == 0) return 0;
     return a / b;
