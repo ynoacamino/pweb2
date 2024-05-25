a9afb5d8586233e0f1fc5113aa3fcc7dfbd7b847
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-05-25 11:16:21 -0500

    agrega el ejercicio f

--- lab05/Tarea-del-Ajedrez/Ejercicio2f.py
+++ lab05/Tarea-del-Ajedrez/Ejercicio2f.py
@@ -1,2 +1,10 @@
 from interpreter import draw
 from chessPictures import *
+
+block = square.join(square.negative())
+
+square_block = block.up(block.negative())
+
+
+
+draw(square_block.horizontalRepeat(4).verticalRepeat(2))
