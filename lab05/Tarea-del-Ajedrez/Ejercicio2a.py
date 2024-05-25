0573b58887d9fe55984c841e03d957c84753bd87
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-05-24 21:03:14 -0500

    Add .gitignore file with updated ignore patterns

---
+++ lab05/Tarea-del-Ajedrez/Ejercicio2a.py
@@ -0,0 +1,7 @@
+from interpreter import draw
+from chessPictures import *
+
+draw(knight.verticalMirror())
+
+print("awda")
+print("111", square)
---
+++ lab05/Tarea-del-Ajedrez/picture.py
@@ -0,0 +1,82 @@
+from colors import *
+class Picture:
+  def __init__(self, img):
+    self.img = img;
+
+  def __eq__(self, other):
+    return self.img == other.img
+
+  def _invColor(self, color):
+    if color not in inverter:
+      return color
+    return inverter[color]
+
+  def verticalMirror(self):
+    """ Devuelve el espejo vertical de la imagen """
+    vertical = []
+    for value in self.img:
+      vertical.append(value[::-1])
+    return vertical
+
+  def negative(self):
+    img = self.img[:]
+
+    for i, line in enumerate(self.img):
+      str = ""
+      for char in line:
+        str += Picture._invColor(self, char)
+      img[i] = str
+
+    return Picture(img)
+
+  def join(self, p):
+    img = self.img[:]
+
+    for i, line in enumerate(self.img):
+      str = line + p.img[i]
+      img[i] = str
+
+    return Picture(img)
+
+  def up(self, p):
+    """ Devuelve una nueva figura poniendo la figura p debajo de la
+        figura actual """
+    img = self.img[:]
+
+    for line in p.img:
+      img.append(line)
+
+    return Picture(img)
+
+  def under(self, p):
+    """ Devuelve una nueva figura poniendo la figura p sobre la
+        figura actual """
+    img = p.img[:]
+    for line in self.img:
+      img.append(line)
+
+    return Picture(img)
+  
+  def horizontalRepeat(self, n):
+    """ Devuelve una nueva figura repitiendo la figura actual al costado
+        la cantidad de veces que indique el valor de n """
+    piece = self.img[:]
+    picture = Picture(piece)
+    for i in range(n-1):
+      picture = picture.join(Picture(piece))
+    return picture
+
+  def verticalRepeat(self, n):
+    piece = self.img[:]
+    picture = Picture(piece)
+
+    for i in range(n-1):
+      picture = picture.up(Picture(piece))
+    return picture
+
+  #Extra: Sólo para realmente viciosos 
+  def rotate(self):
+    """Devuelve una figura rotada en 90 grados, puede ser en sentido horario
+    o antihorario"""
+    return Picture(None)
+
